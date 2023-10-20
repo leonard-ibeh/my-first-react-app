6885ee4c7df4188182ef56976dd0116d91b2087d
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-10-20 06:26:12 +0100

    Getting started with React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,14 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
